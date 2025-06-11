d8d46cdb0c7fa698b92b44b643f971963f07435e
chensoul <ichensoul@gmail.com>
2025-06-11 10:29:47 +0800

    refactor(rose-core): 优化 CharPool 类的代码结构和注释
    - 重新分类字符常量，增加字母、标点符号和特殊字符的注释
    - 为每个字符常量添加详细的注释说明
    - 调整代码格式，提高可读性

--- rose-core/src/main/java/io/github/rose/core/util/CharPool.java
+++ rose-core/src/main/java/io/github/rose/core/util/CharPool.java
@@ -19,52 +19,56 @@ package io.github.rose.core.util;
  * char 常量池
  */
 public abstract class CharPool {
-    // @formatter:off
-    public static final char UPPER_A = 'A';
-    public static final char LOWER_A = 'a';
-    public static final char UPPER_Z = 'Z';
-    public static final char LOWER_Z = 'z';
-    public static final char DOT = '.';
-    public static final char AT = '@';
-    public static final char LEFT_BRACE = '{';
-    public static final char RIGHT_BRACE = '}';
-    public static final char LEFT_BRACKET = '(';
-    public static final char RIGHT_BRACKET = ')';
-    public static final char DASH = '-';
-    public static final char PERCENT = '%';
-    public static final char PIPE = '|';
-    public static final char PLUS = '+';
-    public static final char QUESTION_MARK = '?';
-    public static final char EXCLAMATION_MARK = '!';
-    public static final char EQUALS = '=';
-    public static final char AMPERSAND = '&';
-    public static final char ASTERISK = '*';
-    public static final char STAR = ASTERISK;
-    public static final char BACK_SLASH = '\\';
-    public static final char COLON = ':';
-    public static final char COMMA = ',';
-    public static final char DOLLAR = '$';
-    public static final char SLASH = '/';
-    public static final char HASH = '#';
-    public static final char HAT = '^';
-    public static final char LEFT_CHEV = '<';
-    public static final char NEWLINE = '\n';
-    public static final char NUL = '\0';
-    public static final char N = 'n';
-    public static final char Y = 'y';
-    public static final char QUOTE = '\"';
-    public static final char RETURN = '\r';
-    public static final char TAB = '\t';
-    public static final char RIGHT_CHEV = '>';
-    public static final char SEMICOLON = ';';
-    public static final char SINGLE_QUOTE = '\'';
-    public static final char BACKTICK = '`';
-    public static final char SPACE = ' ';
-    public static final char TILDA = '~';
-    public static final char LEFT_SQ_BRACKET = '[';
-    public static final char RIGHT_SQ_BRACKET = ']';
-    public static final char UNDERSCORE = '_';
-    public static final char ONE = '1';
-    public static final char ZERO = '0';
+    // 字母常量
+    public static final char UPPER_A = 'A'; // 大写字母 A
+    public static final char LOWER_A = 'a'; // 小写字母 a
+    public static final char UPPER_Z = 'Z'; // 大写字母 Z
+    public static final char LOWER_Z = 'z'; // 小写字母 z
+
+    // 标点符号常量
+    public static final char DOT = '.'; // 点号
+    public static final char AT = '@'; // 符号 @
+    public static final char LEFT_BRACE = '{'; // 左大括号
+    public static final char RIGHT_BRACE = '}'; // 右大括号
+    public static final char LEFT_BRACKET = '('; // 左括号
+    public static final char RIGHT_BRACKET = ')'; // 右括号
+    public static final char DASH = '-'; // 破折号
+    public static final char PERCENT = '%'; // 百分号
+    public static final char PIPE = '|'; // 竖线
+    public static final char PLUS = '+'; // 加号
+    public static final char QUESTION_MARK = '?'; // 问号
+    public static final char EXCLAMATION_MARK = '!'; // 感叹号
+    public static final char EQUALS = '='; // 等号
+    public static final char AMPERSAND = '&'; // 与号
+    public static final char ASTERISK = '*'; // 星号
+    public static final char STAR = ASTERISK; // 星号（别名）
+    public static final char BACK_SLASH = '\\'; // 反斜杠
+    public static final char COLON = ':'; // 冒号
+    public static final char COMMA = ','; // 逗号
+    public static final char DOLLAR = '$'; // 美元符号
+    public static final char SLASH = '/'; // 斜杠
+    public static final char HASH = '#'; // 井号
+    public static final char HAT = '^'; // 脱字符
+    public static final char LEFT_CHEV = '<'; // 左尖括号
+    public static final char RIGHT_CHEV = '>'; // 右尖括号
+    public static final char SEMICOLON = ';'; // 分号
+    public static final char SINGLE_QUOTE = '\''; // 单引号
+    public static final char BACKTICK = '`'; // 反引号
+    public static final char SPACE = ' '; // 空格
+    public static final char TILDA = '~'; // 波浪号
+    public static final char LEFT_SQ_BRACKET = '['; // 左方括号
+    public static final char RIGHT_SQ_BRACKET = ']'; // 右方括号
+    public static final char UNDERSCORE = '_'; // 下划线
+
+    // 特殊字符常量
+    public static final char NEWLINE = '\n'; // 换行符
+    public static final char NUL = '\0'; // 空字符
+    public static final char N = 'n'; // 字符 n
+    public static final char Y = 'y'; // 字符 y
+    public static final char QUOTE = '\"'; // 双引号
+    public static final char RETURN = '\r'; // 回车符
+    public static final char TAB = '\t'; // 制表符
+    public static final char ONE = '1'; // 数字 1
+    public static final char ZERO = '0'; // 数字 0
     // @formatter:on
 }
